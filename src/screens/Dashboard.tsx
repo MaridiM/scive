8630d1cbf5b1db7e424dbb4befe1baf9dae93f21
MaridiM <maridim92@gmail.com>
2025-01-20 20:52:32 +0300

    Fix auth

--- src/screens/Dashboard.tsx
+++ src/screens/Dashboard.tsx
@@ -78,7 +78,7 @@ export default function Dashboard() {
 
     return (
         <div className='flex flex-1 justify-between gap-1'>
-            <section className='border-devider flex w-full min-w-[460px] max-w-[640px] flex-col gap-y-1 border-r-[1px] bg-surface-inactive px-6 py-5'>
+            <section className='border-devider flex min-w-[440px] flex-col gap-y-1 border-r-[1px] bg-surface-inactive px-6 py-5 desktop:max-w-[640px]'>
                 <section className='flex h-full max-h-[601px] min-h-[509px] flex-col'>
                     <Header title='Scive Digest AI' counter={{ count: 2, subject: 'new' }} />
 
@@ -264,9 +264,106 @@ export default function Dashboard() {
                 </section>
             </section>
 
-            <section className='border-devider min-w-[536px] flex-1 border-x-[1px] desktop:min-w-[656px]'>CHAT</section>
-            <section className='grid-row-3 border-devider grid min-w-[320px] max-w-[481px] flex-1 border-l-[1px] px-6 py-5'>
-                LEFT
+            <section className='border-devider min-w-[536px] flex-1 flex-grow border-x-[1px] desktop:min-w-[656px]'>
+                CHAT
+            </section>
+            <section className='grid-row-3 border-devider grid min-w-[320px] flex-1 border-l-[1px] px-6 py-5 desktop:max-w-[480px]'>
+                <section>TODO LIST</section>
+                <section>TODO SUGGESTIONS</section>
+                <section>
+                    <Header title='Highlights' />
+                    <section className='flex w-auto flex-1 flex-col gap-4 overflow-hidden py-2'>
+                        <section className='flex h-full flex-1 flex-col overflow-hidden'>
+                            <Typography variant='body' className='!text-text-ultra-light'>
+                                Here you will see details of selected email chain
+                            </Typography>
+
+                            {/* <ul className='flex flex-1 flex-col overflow-y-auto pr-1'>
+                                {DIGESTS.map((item, idx) => {
+                                    let part1 = DIGESTS.slice(0, Math.floor(DIGESTS.length / 3))
+                                    let part2 = DIGESTS.slice(
+                                        Math.floor(DIGESTS.length / 3),
+                                        Math.floor((2 * DIGESTS.length) / 3)
+                                    )
+                                    let part3 = DIGESTS.slice(Math.floor((2 * DIGESTS.length) / 3))
+
+                                    const isReadDigest =
+                                        readDigestItemIds.includes(item.id) && selectedDigestId !== item.id
+                                    const isActiveDigest = item.id === selectedDigestId
+                                    const isHoverDigest = isHoveredId === String(item.id)
+                                    const isHoverReadDigest = isReadDigest && isHoveredId === String(item.id)
+
+                                    if (readDigestItems.includes(String(item.id))) return
+
+                                    return (
+                                        <li
+                                            key={idx}
+                                            className='relative flex min-h-fit flex-row overflow-hidden'
+                                            onMouseLeave={() => setIsHoveredId(null)}
+                                            onMouseEnter={() => setIsHoveredId(String(item.id))}
+                                            onClick={() => digestItemHandler(item.id)}
+                                        >
+                                            <Typography
+                                                variant='body'
+                                                className={cn(
+                                                    'relative mb-2 flex w-fit cursor-default flex-row overflow-hidden rounded-lg bg-white px-2 py-1 font-semibold transition-all duration-150 ease-in-out',
+                                                    isHoverDigest && `bg-white !text-text-ultra-light`,
+                                                    isReadDigest && 'bg-gray-200 font-normal !text-gray-400',
+                                                    isHoverReadDigest && 'bg-gray-200 font-semibold !text-gray-400',
+                                                    isActiveDigest && 'bg-black !text-white',
+
+                                                    part3.includes(item) &&
+                                                        `min-h-fit text-base-body2 leading-5 ${
+                                                            textSize === 'large'
+                                                                ? 'desktop:text-base-body'
+                                                                : 'desktop:text-base-body2'
+                                                        }`,
+                                                    part2.includes(item) &&
+                                                        `min-h-fit text-base-body-digest leading-7 ${
+                                                            textSize === 'large'
+                                                                ? 'desktop:text-base-body-digest1'
+                                                                : 'desktop:text-base-body-digest'
+                                                        }`,
+                                                    part1.includes(item) &&
+                                                        `min-h-fit text-base-body-digest1 leading-[30px] ${
+                                                            textSize === 'large'
+                                                                ? 'desktop:text-base-h3'
+                                                                : 'desktop:text-base-body-digest1'
+                                                        }`
+                                                )}
+                                            >
+                                                {item.content}
+                                                {isHoverDigest && (
+                                                    <Button
+                                                        variant='clear'
+                                                        className={cn(
+                                                            'absolute left-0 top-0 flex h-full w-[32px] rounded-none bg-button px-0'
+                                                        )}
+                                                        onClick={() => digestItemDoneHandler(item.id)}
+                                                    >
+                                                        <svg
+                                                            width='28px'
+                                                            height='28px'
+                                                            viewBox='0 0 36 36'
+                                                            fill='none'
+                                                            xmlns='http://www.w3.org/2000/svg'
+                                                        >
+                                                            <path
+                                                                d='M11.25 17.25L16.25 22.25L24.75 13.75'
+                                                                stroke='#ffff'
+                                                                strokeWidth='5'
+                                                            />
+                                                        </svg>
+                                                    </Button>
+                                                )}
+                                            </Typography>
+                                        </li>
+                                    )
+                                })}
+                            </ul> */}
+                        </section>
+                    </section>
+                </section>
             </section>
         </div>
     )
