12b1caa0f713c22b672ce48fc819e2580c8d886a
MaridiM <maridim92@gmail.com>
2025-01-29 16:29:48 +0300

    Update navbar, add mailbox header, add base path filtration

--- src/shared/config/paths.config.ts
+++ src/shared/config/paths.config.ts
@@ -2,9 +2,12 @@ export const paths = {
     home: '/',
     auth: '/auth',
     dashboard: '/dashboard',
-    mailbox: (label: string = '', id: string = ''): string =>
+    mailbox: (label: TMailboxPaths = 'new', id: string = ''): string =>
         `/mailbox${(id || label) && '?'}${label && `label=${label}${id && '&'}`}${id && `id=${id}`}`,
     todos: (filter: string = '', id: string = ''): string =>
         `/todos${(id || filter) && '?'}${filter && `filter=${filter}${id && '&'}`}${id && `id=${id}`}`,
-    settings: (path: 'plans' | 'billings' | 'notifications' | '' = '') => `/settings${path && '?path='}${path}`
+    settings: (path: TSettingsPaths = 'account') => `/settings${path && '?path='}${path}`
 }
+
+export type TMailboxPaths = 'new' | 'inbox' | 'sent' | 'drafts' | 'spam' | 'trash' | ''
+export type TSettingsPaths = 'account' | 'plans' | 'billings' | 'notifications' | ''
